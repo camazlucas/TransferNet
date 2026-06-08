b5adb63583fcc4163e948e6d19dce4658951ce31
Lucas Camaz Ferreira <camazlucas@gmail.com>
2026-06-08 14:04:43 -0300

    Testando predict_paths

---
+++ AtualizacaoMetaQA/predict_paths.py
@@ -0,0 +1,205 @@
+import os
+import torch
+import torch.nn as nn
+import argparse
+from tqdm import tqdm
+from collections import defaultdict
+from utils.misc import batch_device
+from .data import load_data
+from .model import TransferNet
+
+from collections import defaultdict
+import json
+
+from IPython import embed
+
+
+def validate(args, model, data, device, kg_index, verbose = False):
+    model.eval()
+    count = 0
+    correct = 0
+    hop_count = defaultdict(list)
+    results = []
+    with torch.no_grad():
+        for batch in tqdm(data, total=len(data)):
+            outputs = model(*batch_device(batch, device)) # [bsz, Esize]
+
+            ########################## Extracao das Probabilidades
+            
+            rel_probs = outputs["rel_probs"][0]
+
+            
+            ########################### DEBUG #########################
+            print(outputs["rel_probs"][0].shape)
+            embed()
+            ###########################################################
+
+            top_scores, top_rel_ids = torch.topk(
+                rel_probs,
+                k=3,
+                dim=1
+            )
+
+            for i in range(len(top_rel_ids)):
+
+                question_ids = batch[1]["input_ids"][i].tolist()
+
+                question = data.tokenizer.decode(
+                    question_ids,
+                    skip_special_tokens=True
+                )
+
+                head_id = batch[0][i].argmax().item()
+
+                item = {
+                    "question": question,
+                    "head_entity": data.id2ent[head_id],
+                    "top_paths": []
+                }
+
+                for rank in range(3):
+
+                    rel_id = top_rel_ids[i][rank].item()
+
+                    rel_score = float(
+                        top_scores[i][rank].item()
+                    )
+
+                    tails = kg_index.get(
+                        (head_id, rel_id),
+                        []
+                    )
+
+                    triples_json = []
+
+                    for tail_id in tails:
+
+                        triples_json.append([
+                            data.id2ent[head_id],
+                            data.id2rel[rel_id],
+                            data.id2ent[tail_id]
+                        ])
+
+                    item["top_paths"].append({
+                        "rank": rank + 1,
+                        "path_score": rel_score,
+                        "triples": triples_json
+                    })
+
+                results.append(item)
+            
+            ######################################################
+
+            e_score = outputs['e_score'].cpu()
+            scores, idx = torch.max(e_score, dim = 1) # [bsz], [bsz]
+            match_score = torch.gather(batch[2], 1, idx.unsqueeze(-1)).squeeze().tolist()
+            count += len(match_score)
+            correct += sum(match_score)
+            for i in range(len(match_score)):
+                h = outputs['hop_attn'][i].argmax().item()
+                hop_count[h].append(match_score[i])
+
+            if verbose:
+                answers = batch[2]
+                for i in range(len(match_score)):
+                    if match_score[i] == 0:
+                        print('================================================================')
+                        question_ids = batch[1]['input_ids'][i].tolist()
+                        question_tokens = data.tokenizer.convert_ids_to_tokens(question_ids)
+                        print(' '.join(question_tokens))
+                        topic_id = batch[0][i].argmax(0).item()
+                        print('> topic entity: {}'.format(data.id2ent[topic_id]))
+                        for t in range(2):
+                            print('>>>>>>> step {}'.format(t))
+                            tmp = ' '.join(['{}: {:.3f}'.format(x, y) for x,y in 
+                                zip(question_tokens, outputs['word_attns'][t][i].tolist())])
+                            print('> Attention: ' + tmp)
+                            print('> Relation:')
+                            rel_idx = outputs['rel_probs'][t][i].gt(0.9).nonzero().squeeze(1).tolist()
+                            for x in rel_idx:
+                                print('  {}: {:.3f}'.format(data.id2rel[x], outputs['rel_probs'][t][i][x].item()))
+
+                            print('> Entity: {}'.format('; '.join([data.id2ent[_] for _ in outputs['ent_probs'][t][i].gt(0.8).nonzero().squeeze(1).tolist()])))
+                        print('----')
+                        print('> max is {}'.format(data.id2ent[idx[i].item()]))
+                        print('> golden: {}'.format('; '.join([data.id2ent[_] for _ in answers[i].gt(0.9).nonzero().squeeze(1).tolist()])))
+                        print('> prediction: {}'.format('; '.join([data.id2ent[_] for _ in e_score[i].gt(0.9).nonzero().squeeze(1).tolist()])))
+                        print(' '.join(question_tokens))
+                        print(outputs['hop_attn'][i].tolist())
+                        embed()
+    acc = correct / count
+    print(acc)
+    print('pred hop accuracy: 1-hop {} (total {}), 2-hop {} (total {})'.format(
+        sum(hop_count[0])/(len(hop_count[0])+0.1),
+        len(hop_count[0]),
+        sum(hop_count[1])/(len(hop_count[1])+0.1),
+        len(hop_count[1]),
+        ))
+    with open(
+            "predicted_paths.json",
+            "w",
+            encoding="utf-8"
+        ) as f:
+
+        ##################### DEBUG #################
+
+        if results:
+            print(json.dumps(results[0], indent=2, ensure_ascii=False))
+
+        #############################################
+
+
+        json.dump(
+            results,
+            f,
+            ensure_ascii=False,
+            indent=2
+        )
+
+    return acc
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    # input and output
+    parser.add_argument('--input_dir', default = './input')
+    parser.add_argument('--ckpt', required = True)
+    parser.add_argument('--mode', default='val', choices=['val', 'vis', 'test'])
+    parser.add_argument('--bert_name', default='bert-base-uncased')
+    args = parser.parse_args()
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    ent2id, rel2id, triples, train_loader, val_loader = load_data(
+    args.input_dir,
+    'bert-base-uncased',
+    16
+    )
+    #################################### Lista de Indices
+    # data.id2ent = {v:k for k,v in ent2id.items()}
+    # data.id2rel = {v:k for k,v in rel2id.items()}
+
+    kg_index = defaultdict(list)
+
+    for s, p, o in triples.tolist():
+        kg_index[(s, p)].append(o)
+    #####################################################
+
+    model = TransferNet(args, ent2id, rel2id, triples)
+    missing, unexpected = model.load_state_dict(torch.load(args.ckpt), strict=False)
+    if missing:
+        print("Missing keys: {}".format("; ".join(missing)))
+    if unexpected:
+        print("Unexpected keys: {}".format("; ".join(unexpected)))
+    model = model.to(device)
+    # model.triples = model.triples.to(device)
+    model.Msubj = model.Msubj.to(device)
+    model.Mobj = model.Mobj.to(device)
+    model.Mrel = model.Mrel.to(device)
+
+    if args.mode == 'vis':
+        validate(args, model, val_loader, device, kg_index, True)
+    elif args.mode == 'val':
+        validate(args, model, val_loader, device, kg_index, False)
+
+if __name__ == '__main__':
+    main()
