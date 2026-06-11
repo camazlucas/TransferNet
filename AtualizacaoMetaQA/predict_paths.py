c611d546964dfa6988eb5116363175f5b0158ed2
Lucas Camaz Ferreira <camazlucas@gmail.com>
2026-06-11 15:04:24 -0300

    Ajuste do nome do json, ajustar depois para arg pass

--- AtualizacaoMetaQA/predict_paths.py
+++ AtualizacaoMetaQA/predict_paths.py
@@ -162,7 +162,7 @@ def validate(args, model, data, device, kg_index, verbose = False):
     )
 
     with open(
-            "predicted_paths.json",
+            "predicted_paths_3hop.json",
             "w",
             encoding="utf-8"
         ) as f:
