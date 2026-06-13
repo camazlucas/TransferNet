5d4b6efb80fdf60182be2c3c733547373995d7a7
Lucas Camaz Ferreira <camazlucas@gmail.com>
2026-06-13 19:09:35 -0300

    Agregacao dos datasets para treinamento geral

--- AtualizacaoMetaQA/predict_paths.py
+++ AtualizacaoMetaQA/predict_paths.py
@@ -99,34 +99,6 @@ def validate(args, model, data, device, kg_index, verbose = False):
 
                         new_paths = new_paths[:100]
 
-                    ######################## DEBUG ################################
-
-                    print("\n======================")
-                    print("QUESTION:", question)
-                    print("HOP:", hop + 1)
-
-                    print("\nCURRENT ENTITIES:")
-                    for path in current_paths:
-                        print(data.id2ent[path["entity"]])
-
-                    print("\nTOP 10 RELATIONS:")
-
-                    top_scores, top_rel_ids = torch.topk(
-                        rel_probs,
-                        k=10
-                    )
-
-                    for score, rel_id in zip(top_scores, top_rel_ids):
-                        print(
-                            data.id2rel[rel_id.item()],
-                            float(score)
-                        )
-
-                    print("\nSELECTED RELATIONS:")
-                    for rel_id in selected_rel_ids:
-                        print(data.id2rel[rel_id])
-                    
-                    #############################################################
 
                     if len(new_paths) == 0:
                         break
